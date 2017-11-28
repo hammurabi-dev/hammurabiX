d56ce140f311aef837906bac8b6b6a316ec31a5e
Jiaxin Wang <jiaxin@Jiaxins-MacBook-Pro.local>
2017-11-28 21:13:09 +0100

    recover wmap lsa

--- src/fields/gmf/breg_wmap.cc
+++ src/fields/gmf/breg_wmap.cc
@@ -15,12 +15,11 @@ using namespace std;
 /* wmap-3yr */
 vec3_t<double> Breg_wmap::breg(const vec3_t<double> &pos,Param *par){
     const double r {sqrt(pos.x*pos.x + pos.y*pos.y)};
-    if (r>(20.*CGS_U_kpc) or r<(3.*CGS_U_kpc )) {
+    if (r>(20.*CGS_U_kpc) or r<(3.*CGS_U_kpc)) {
         return vec3_t<double> {0.,0.,0.};
     }
     // units
-    const double orient {-1}; // -1 means conterclockwise
-    const double b0 {orient*par->breg_wmap.b0};
+    const double b0 {par->breg_wmap.b0};
     const double psi0 {par->breg_wmap.psi0};
     const double psi1 {par->breg_wmap.psi1};
     const double chi0 {par->breg_wmap.chi0};
@@ -28,7 +27,6 @@ vec3_t<double> Breg_wmap::breg(const vec3_t<double> &pos,Param *par){
     const double sin_p {pos.y/r};
     const double psi {psi0 + psi1*log(r/(8.*CGS_U_kpc))};
     const double chi {chi0*tanh(pos.z/(1.*CGS_U_kpc))};
-    
     return vec3_t<double> {
         b0*(sin(psi)*cos(chi)*cos_p - cos(psi)*cos(chi)*sin_p),
         b0*(sin(psi)*cos(chi)*sin_p + cos(psi)*cos(chi)*cos_p),
